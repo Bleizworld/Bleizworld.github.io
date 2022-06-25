ed18aa0401e80fce71caf8b6fa3abecff0563aee
Solomon Eniola Adegoke <souceking@gmail.com>
2022-06-25 18:42:55 +0100

    my assignment

---
+++ calculator/calculator.js
@@ -0,0 +1,20 @@
+let num1 = parseInt(prompt("Enter first number: "));
+let oper = prompt("Enter the operator: ");
+let num2 = parseInt(prompt("Enter the second number: "));
+
+if(oper == "+")
+	{ alert(num1 + num2);
+	}
+	 else if(oper == "-");
+	{
+		alert(num1 - num2);
+
+	}
+	else if(oper == "*")
+	{
+		alert(num1 * num2);
+	}
+	else if (oper == "/")
+	{
+		alert(num1 / num2);
+	}
---
+++ details/details.js
@@ -0,0 +1,6 @@
+let name = "Name: Oyelakin Blessing Deborah";
+let country = "Country: Nigeria";
+let height = "Height: 153";
+alert(name);
+alert(country);
+alert(height);
